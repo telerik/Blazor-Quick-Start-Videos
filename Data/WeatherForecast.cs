0664dfea6e4486dfb298a0c71544ee17f213da40
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-07-16 11:17:10 -0400

    Added demo code

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -11,5 +11,8 @@ namespace BlazorWeatherComponentDemo.Data
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public bool Selected { get; set; } = false;
+
     }
 }
